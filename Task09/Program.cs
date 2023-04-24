da89a400a39750762f245b07821fe761f32ddf13
FeNiksleks <feniksleks@yandex.ru>
2023-04-24 21:03:03 +0300

    Написали метод Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -8,8 +8,8 @@
 int number = new Random().Next(10, 100); // (10, 99 + 1)
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
 // if (firstDigit > secondDigit) 
 // {
@@ -20,5 +20,18 @@ int secondDigit = number % 10;
 //     Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
 // }
 
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Тернарный оператор
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit = MaxDigit(number);
 Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+// Метод
+int MaxDigit (int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    // int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
