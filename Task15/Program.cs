96536e199fad2276049318d55e7c4568fe05da9e
FeNiksleks <feniksleks@yandex.ru>
2023-04-26 21:01:31 +0300

    Выполнил Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -3,9 +3,20 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Введите день недели");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number <=5)
+// Console.WriteLine("Введите день недели");
+// int number = Convert.ToInt32(Console.ReadLine());
+int number;
+do {
+    Console.WriteLine("Введите день недели ");
+    Int32.TryParse(Console.ReadLine(), out number);
+} while(number > 7);
+// Console.WriteLine("Это не день недели");
+// if (number > 7)
+// {
+//     Console.WriteLine("Это не день недели");
+//     restart programm
+// }
+if (number <= 5)
 {
     Console.WriteLine($"{number} -> нет ");
 }
@@ -13,3 +24,12 @@ else
 {
     Console.WriteLine($"{number} -> да ");
 }
+
+
+
+// int digit;
+// do {
+//     Console.WriteLine("Введите возраст: ");
+//     Int32.TryParse(Console.ReadLine(), out digit);
+// } while(digit < 14 || digit > 85);
+// Console.WriteLine(digit);
