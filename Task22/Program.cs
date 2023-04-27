1aff5871746b5a25b5047a716f46a2d182086dc8
FeNiksleks <feniksleks@yandex.ru>
2023-04-27 22:13:57 +0300

    Выполнил Task22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+Square(number, degree);
+void Square (int num, int pow)
+{
+        int count = 1;
+    while (count <= num)
+{
+    
+        Console.WriteLine($"{count,3}  {Math.Pow(count, pow),5}"); //Math.Pow((x2 - x1), 2)
+            
+    count++;
+}
+}
