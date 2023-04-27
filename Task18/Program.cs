bc87bea94027091f193442a7857333544fd5a1d4
FeNiksleks <feniksleks@yandex.ru>
2023-04-27 21:03:08 +0300

    Выполнил Task18 внесли изменения string

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -5,7 +5,8 @@
 
 Console.WriteLine("Введите номер четверти координат");
 Console.Write("Четверть №  ");
-int quarter = Convert.ToInt32(Console.ReadLine());
+// int quarter = Convert.ToInt32(Console.ReadLine());
+string quarter = Console.ReadLine();
 
 string coordinate = Range (quarter);
 string result = coordinate != null 
@@ -13,11 +14,16 @@ string result = coordinate != null
                 : "Введена некорректная четверть";
 Console.WriteLine(result);
 
-string Range (int q)
+// string Range (int q)
+string Range (string q)
 {
-    if (q == 1) return "x > 0 && y > 0";
-    if (q == 2) return "x < 0 && y > 0";
-    if (q == 3) return "x < 0 && y < 0";
-    if (q == 4) return "x > 0 && y < 0";
+    if (q == "1") return "x > 0 && y > 0";
+    if (q == "2") return "x < 0 && y > 0";
+    if (q == "3") return "x < 0 && y < 0";
+    if (q == "4" || q == "четыре") return "x > 0 && y < 0";
+    // if (q == 1) return "x > 0 && y > 0";
+    // if (q == 2) return "x < 0 && y > 0";
+    // if (q == 3) return "x < 0 && y < 0";
+    // if (q == 4) return "x > 0 && y < 0";
     return null;
 }
