516115138a822e5aba23faf23fb43f65543cfba3
FeNiksleks <feniksleks@yandex.ru>
2023-05-18 20:58:13 +0300

    Выполнил Task26

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int QuantityNumber (int num)
+{
+    int count = 0;
+    while(num !=0)
+    {
+        num /=10;
+        count++;
+    }
+    return count;
+}
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = QuantityNumber (number);
+Console.WriteLine($"{number} => {result}");
+
