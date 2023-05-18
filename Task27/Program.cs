346b6767f515afb995042e3541c225a46c1b4514
FeNiksleks <feniksleks@yandex.ru>
2023-05-18 22:23:10 +0300

    Выполнил Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int NumberLen(int a)
+{
+    int count = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        count++;
+    }
+    return count;
+}
+
+int SummerNumbers(int a, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = NumberLen(number);
+int result = SummerNumbers(number, count);
+Console.WriteLine($"{number} => {result}");
