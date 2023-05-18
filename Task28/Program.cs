8592f819a7e2506a30705701800194120df5eb83
FeNiksleks <feniksleks@yandex.ru>
2023-05-18 21:18:46 +0300

    Выполнил Task28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int mulNumbers = MultiplicationNumbers(number);
+Console.WriteLine($"{number} -> {mulNumbers}");
+
+int MultiplicationNumbers(int num)
+{
+    int mul = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mul = mul * i;
+    }
+    return mul;
+}
