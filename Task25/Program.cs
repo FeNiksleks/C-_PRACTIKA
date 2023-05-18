5eca8fbb8b33bae00e494b98b7fc305bfb257a26
FeNiksleks <feniksleks@yandex.ru>
2023-05-18 21:31:49 +0300

    Выполнил Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Square(numberA, numberB);
+void Square (int num, int pow)
+{
+   
+        Console.WriteLine($"{num} в степени {pow} -> {Math.Pow(num, pow)}");
+    }
